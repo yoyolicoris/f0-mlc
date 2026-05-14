32a64ab7f1ad011cd4a90ab95258241d64c727fb
Chin-Yun Yu <chin-yun.yu@qmul.ac.uk>
2026-05-14 21:19:11 +0800

    fix: set weights_only to fase

--- src/train.py
+++ src/train.py
@@ -86,7 +86,12 @@ def train(cfg: DictConfig) -> Tuple[Dict[str, Any], Dict[str, Any]]:
 
     if cfg.get("train"):
         log.info("Starting training!")
-        trainer.fit(model=model, datamodule=datamodule, ckpt_path=cfg.get("ckpt_path"))
+        trainer.fit(
+            model=model,
+            datamodule=datamodule,
+            ckpt_path=cfg.get("ckpt_path"),
+            weights_only=False,
+        )
 
     train_metrics = trainer.callback_metrics
 
