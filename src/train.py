e684ada036e33c8e1e96475bb034ebe9dde757cc
Chin-Yun Yu <chin-yun.yu@qmul.ac.uk>
2026-05-14 23:19:28 +0800

    fix: set weights_only to False in trainer.test call

--- src/train.py
+++ src/train.py
@@ -101,7 +101,9 @@ def train(cfg: DictConfig) -> Tuple[Dict[str, Any], Dict[str, Any]]:
         if ckpt_path == "":
             log.warning("Best ckpt not found! Using current weights for testing...")
             ckpt_path = None
-        trainer.test(model=model, datamodule=datamodule, ckpt_path=ckpt_path)
+        trainer.test(
+            model=model, datamodule=datamodule, ckpt_path=ckpt_path, weights_only=False
+        )
         log.info(f"Best ckpt path: {ckpt_path}")
 
     test_metrics = trainer.callback_metrics
